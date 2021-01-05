daa755ac7c2a640f4bc4928520db67ce1d9ae159
Marde Fasma <mardefasma123up@gmail.com>
2021-01-05 15:55:07 +0700

    first migration pug

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home.landing.home');
 });
 
 Route::get('/logout', 'Auth\LoginController@logout');
