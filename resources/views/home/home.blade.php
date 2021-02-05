425a7df8212e6fefde01735c40dbfaaac3d2e482
Marde Fasma <mardefasma123up@gmail.com>
2021-02-05 14:45:24 +0700

    badge tools ok

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -94,10 +94,27 @@
                                     </div>
                                     <h3>{{$statistisi_baik->st_user->cover_letter}}</h3>
                                     <div class="content-info">
-                                        <div class="rating-wrapper"><span class="gig-rating text-body-2">
+                                        <div class="rating-wrapper">
+                                            <span class="gig-rating text-body-2">
                                                 <svg xmlns="http://www.w3.org/2000/svg" viewbox="0 0 1792 1792" width="15" height="15">
                                                     <path fill="currentColor" d="M1728 647q0 22-26 48l-363 354 86 500q1 7 1 20 0 21-10.5 35.5t-30.5 14.5q-19 0-40-12l-449-236-449 236q-22 12-40 12-21 0-31.5-14.5t-10.5-35.5q0-6 2-20l86-500-364-354q-25-27-25-48 0-37 56-46l502-73 225-455q19-41 49-41t49 41l225 455 502 73q56 9 56 46z"></path>
-                                                </svg> 5.0<span>(7)</span></span></div>
+                                                </svg> 5.0<span>(7)</span>
+                                            </span>
+                                            @foreach ($statistisi_baik->st_user->tools as $tool)
+                                            @switch($tool)
+                                            @case('SPSS')
+                                            <span class="badge badge-primary">SPSS</span>
+                                            @break
+
+                                            @case('R')
+                                            <span class="badge badge-danger">R</span>
+                                            @break
+
+                                            @default
+                                            <span class="badge badge-secondary">Python</span>
+                                            @endswitch
+                                            @endforeach
+                                        </div>
                                     </div>
                                     <div class="footer"><i class="fa fa-heart" aria-hidden="true"></i></div>
                                 </div>
