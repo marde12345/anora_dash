899c77745122b9290acad50dde0a48d888bdfb41
Marde Fasma <mardefasma123up@gmail.com>
2021-02-05 13:28:05 +0700

    fix query home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -27,7 +27,7 @@ class HomeController extends Controller
     {
         $users = User::count();
 
-        $statistisi_terbaik = UserResource::collection(User::all()->random(10));
+        $statistisi_terbaik = UserResource::collection(User::where('role', 'st_user')->inRandomOrder()->limit(10)->get());
         $statistisi_terbaik = json_decode(json_encode($statistisi_terbaik));
 
         $widget = [
