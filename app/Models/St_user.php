86ed52f4399ee3a4e61cfe75ddb59a369d4026bc
Marde Fasma <mardefasma123up@gmail.com>
2021-02-08 22:20:07 +0700

    private model st_user tools and services

--- app/Models/St_user.php
+++ app/Models/St_user.php
@@ -8,5 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class St_user extends Model
 {
     use HasFactory;
+
+    private const TOOLS = ['SPSS', 'Python', 'R'];
+    private const SERVICES = ['Analisis Regresi', 'Olah Data', 'Data Entry', 'Pembuatan Kuisioner', 'Konsultasi Statistik'];
+
     protected $guarded = []; //tambahkan baris ini
+
+    // Per get an
+    public static function getTools()
+    {
+        return self::TOOLS;
+    }
+
+    public static function getServices()
+    {
+        return self::SERVICES;
+    }
 }
